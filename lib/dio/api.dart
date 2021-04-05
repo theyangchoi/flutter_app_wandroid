5fab0580d3c7fa123252c702d5eb8ed474f3607e
theyangchoi <theyangchoi@163.com>
2021-04-05 13:59:30 +0800

    commit on 2021/4/5,first update

--- lib/dio/api.dart
+++ lib/dio/api.dart
@@ -4,14 +4,20 @@ import 'package:flutter_app_wandroid/dio/dio_mamager.dart';
 typedef void OnResult(Map<String,dynamic> data);
 
 class Api{
+  ///根地址
   static const Base_Url = "https://www.wanandroid.com/";
+  /// API 地址
+  ///首页文章列表
   static const String ARTICLE_LIST = "article/list/";
+  ///首页banner列表
   static const String Banner = "banner/json";
 
+  ///API实现
+  ///获取首页文章列表  使用$符号拼接地址ARTICLE_LIST  以及  页码 page
   static getArticleList(int page) async {
     return DioManager().get('$ARTICLE_LIST$page/json');
   }
-
+  ///获取首页banner列表
   static getBanner() async {
     return await DioManager().get(Banner);
   }
