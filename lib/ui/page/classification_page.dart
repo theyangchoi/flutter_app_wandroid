e1e19a1da4ff04c38c3c0f78cbd52d59071bc1d0
theyangchoi <theyangchoi@163.com>
2021-04-12 11:58:20 +0800

    commint on 2021/4/12,分类页面编写

--- lib/ui/page/classification_page.dart
+++ lib/ui/page/classification_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_app_wandroid/network/common/global.dart';
 import 'package:flutter_app_wandroid/network/model/classification_model.dart';
 import 'package:flutter_app_wandroid/network/view_model/classification_view_model.dart';
 import 'package:flutter_app_wandroid/ui/widget/easyRefresh_widget.dart';
+import 'package:flutter_app_wandroid/utils/flobal_config.dart';
 
 /**
  * 分类页面
@@ -16,94 +17,115 @@ class ClassificationPage extends StatefulWidget {
 }
 
 class _ClassificationPageState extends State<ClassificationPage> {
-
-  final ClassificationViewModel _classificationViewModel = ClassificationViewModel();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Row(
-          children: <Widget>[
-            Container(
-              child: CustomScrollView(
-                slivers: <Widget>[
-                  SliverToBoxAdapter(
-                    child: ClassificationLeftWidget(),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-
-      ),
-    );
-  }
-}
-/**
- * 分类左边的数据
- */
-class ClassificationLeftWidget extends StatefulWidget {
-
-
-  ClassificationLeftWidget();
-
-  @override
-  _ClassificationLeftWidgetState createState() => _ClassificationLeftWidgetState();
-}
-
-class _ClassificationLeftWidgetState extends State<ClassificationLeftWidget> {
-  ClassificationViewModel _viewModel = ClassificationViewModel();
-  //当前选中索引
   int menuPosition = 0;
+  final ClassificationViewModel _classificationViewModel = ClassificationViewModel();
+  List<Color> colors = GlobalConfig.colors;
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     menuPosition = 0;
   }
+
   @override
   Widget build(BuildContext context) {
     return BasePage<ClassificationViewModel>(
-      viewModel: _viewModel,
+      viewModel: _classificationViewModel,
       onFirstLoading: (v){
         v.getTreeList();
       },
       builder: (context,model,child){
         return BaseWidget(
           reqStatus: model.reqStatus,
-          child: ListView.builder(
-              itemCount: model.getClassList.length,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),//上下滑动
-              itemBuilder: (context,index){
-                ClassificationModel dataModel = model.getClassList[index];
-                return GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      menuPosition = index;
-                    });
-                  },
+          child: Row(
+            children: <Widget>[
+              //left
+              Container(
+                constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width / 3, //宽度
+                    minWidth: MediaQuery.of(context).size.width / 3,
+                    minHeight: double.infinity),
+                child: CustomScrollView(
+                  slivers: <Widget>[
+                    SliverToBoxAdapter(
+                      child: ListView.builder(
+                          itemCount: model.getClassList != null ? model.getClassList.length : 0,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context,index){
+                            ClassificationModel dataModel = model.getClassList[index];
+                            return GestureDetector(
+                              onTap: (){
+                                setState(() {
+                                  menuPosition = index;
+                                });
+                              },
+                              child: Container(
+                                height: 50,
+                                width: winWidth,
+                                child: Column(//使用column的mainAxisAlignment属性使文字垂直居中
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      dataModel.name,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 16,color: index == menuPosition ? Colors.blue : Colors.black),
+                                    )
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(width: 0.5,color: Colors.grey)),
+                                    color: index == menuPosition ? Colors.grey[200] : Colors.white
+                                ),
+                              ),
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              ),
+              //right
+              Expanded(
+                  flex: 1,
                   child: Container(
-                    height: 50,
-                    width: winWidth,
-                    child: Text(
-                      dataModel.name,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16,color: index == menuPosition ? Colors.blue : Colors.black),
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(width: 0.5,color: Colors.grey)),
-                        color: index == menuPosition ? Colors.grey[200] : Colors.white
-                    ),
-                  ),
-                );
-              }
+                    padding: EdgeInsets.all(10),
+                    child: GridView.builder(
+                        itemCount: model.getClassList[menuPosition].children.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount:2,
+                          crossAxisSpacing:10,
+                          mainAxisSpacing: 10,
+                          childAspectRatio:(MediaQuery.of(context).size.width / 3 * 2 - 20) /2 /50
+                        ),
+                        itemBuilder: (context,index){
+                          ClassificationModel dataModel = model.getClassList[index];
+                          return GestureDetector(
+                            onTap: (){},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                              alignment: Alignment.center,
+                              child: Text(
+                                model.getClassList[menuPosition].children[index]['name'],
+                                textAlign: TextAlign.center,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: colors[index % colors.length]),
+                              ),
+                            ),
+                          );
+                        }),
+                  )
+              )
+            ],
           ),
         );
       },
     );
   }
 }
-
 
---
+++ lib/utils/flobal_config.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GlobalConfig {
+
+  static bool isDebug = true;
+  static Color themeColor = Colors.blue;
+  static List<Color> colors = [
+    Color(0xffe54d42),
+    Color(0xfff37b1d),
+    Color(0xfffbbd08),
+    Color(0xff8dc63f),
+    Color(0xff39b54a),
+    Color(0xff1cbbb4),
+    Color(0xff0081ff),
+    Color(0xff6739b6),
+    Color(0xff9c26b0),
+    Color(0xffe03997),
+    Color(0xffa5673f)
+  ];
+}
